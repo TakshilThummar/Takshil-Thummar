8619eed4f833be0f63348df30bcbe1004e05a5dd
Takshil <thummartakshil003@gmail.com>
2024-12-16 15:03:29 +0530

    Portfolio Website

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo } from 'react';
 import './Home.css';
-import heroImage from '../../Assets/images/hero.png'
+import heroImage from '../../Assets/images/hero.png';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -45,39 +45,35 @@ const Home = () => {
 
   return (
     <div className='container'>
-
       <div className="home">
-
         <div className="content">
-
           <h2>Heyy There!, <br /> I'm <span>Takshil Thummar</span></h2>
           <h2>I am a <span>{text}</span><span className="cursor">|</span></h2>
 
-          <p>My name is Takshil Thummar and and I'm from Junagadh.</p>
+          <p>My name is Takshil Thummar and I'm from Junagadh.</p>
           <p>Now, My 5th sem is pursuing in BCA (Bachelor's of Computer application) at Noble University - Junagadh.</p>
 
           <div className="buttons">
             <Link to="/about" className="btn">About Me<i className="fas fa-arrow-circle-right"></i></Link>
-            <Link href="/DevOps_Engineer_Takshil.pdf" download="DevOps_Engineer_Takshil.pdf" className="btn">Resume</Link>
+            {/* Use <a> tag for resume download */}
+            <a href="/DevOps_Engineer_Takshil.pdf" download="DevOps_Engineer_Takshil.pdf" className="btn">
+              Resume
+            </a>
           </div>
 
           <div className="socials">
-                <ul className="social-icons">
-                    <li><a className="linkedin" aria-label="LinkedIn" href="https://www.linkedin.com/in/takshil-thummar-158a51303" target="_blank"><i className="fab fa-linkedin"></i></a></li>
-                    <li><a className="github" aria-label="GitHub" href="https://github.com/TakshilThummar" target="_blank"><i className="fab fa-github"></i></a></li>
-                    <li><a className="twitter" aria-label="Twitter" href="#" target="_blank"><i className="fab fa-twitter"></i></a></li>
-                </ul>
-            </div>
-
+            <ul className="social-icons">
+              <li><a className="linkedin" aria-label="LinkedIn" href="https://www.linkedin.com/in/takshil-thummar-158a51303" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a></li>
+              <li><a className="github" aria-label="GitHub" href="https://github.com/TakshilThummar" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></li>
+              <li><a className="twitter" aria-label="Twitter" href="#" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a></li>
+            </ul>
+          </div>
         </div>
-
 
         <div className="image">
           <img draggable="false" className="tilt" src={heroImage} alt='' />
         </div>
-
       </div>
-
     </div>
   );
 }
